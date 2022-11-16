e5cd0270e81cfeedfc68fb8003c0f9302e43656a
Mateo Ortega <mateo041ortega@gmail.com>
2022-11-16 00:41:50 -0700

    First

---
+++ main/inc/spiffs.h
@@ -0,0 +1,20 @@
+#ifndef _SPIFFS_H
+#define _SPIFFS_H
+
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_spiffs.h"
+#include "mbedtls/md5.h"
+#define TAG "wifi"
+
+
+
+void spiffs();
+
+
+#endif _SPIFFS_H
